9a7f1b191ede66d784a2a086b889be97f321295d
Payal Ivare <78032235+PayalIvare@users.noreply.github.com>
2023-12-11 19:05:26 +0530

    Update 17th.py

--- 17th.py
+++ 17th.py
@@ -20,3 +20,35 @@ sorted_percentage = bucket_sort(percentage)
 print("top five scores:")
 for i in range(5):
     print(sorted_percentage[i])
+__________________________________________________________________________________________________________________________________
+python
+# Take input from the user
+n = int(input("How many students' percentages do you want to store? "))
+percentages = []
+
+# Loop to get the percentages
+for i in range(n):
+    percentage = float(input(f"Enter the percentage for student {i+1}: "))
+    percentages.append(percentage)
+
+# Bucket sort function
+def bucket_sort(arr):
+    n = len(arr)
+    buckets = [[] for _ in range(n)]
+
+    for num in arr:
+        index = int(num * n)
+        buckets[index].append(num)
+
+    sorted_arr = []
+    for bucket in buckets:
+        sorted_arr.extend(sorted(bucket))
+
+    return sorted_arr
+
+# Sort the array using bucket sort
+sorted_percentages = bucket_sort(percentages)
+
+# Display the top five scores
+top_five_scores = sorted_percentages[-5:]
+print("The top five scores are:", top_five_scores)
